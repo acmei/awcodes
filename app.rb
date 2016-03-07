67ca9b4d2f3b34efdc129a65c76676b640321f92
anita <asovay@gmail.com>
2016-03-06 20:11:59 -0800

    homepage, remove images, styling

--- app.rb
+++ app.rb
@@ -7,24 +7,4 @@ class PersonalSite < Sinatra::Base
   get "/" do
     erb :index
   end
-
-  # get "/projects" do
-  #   @title = "Projects"
-  #   erb :projects
-  # end
-
-  # get "/contact" do
-  #   @title = "Contact"
-  #   erb :contact
-  # end
-
-  # get "/blog" do
-  #   @title = "Blog"
-  #   erb :blog
-  # end
-
-  # get "/photography" do
-  #   @title = "Photography"
-  #   erb :photography
-  # end
 end
